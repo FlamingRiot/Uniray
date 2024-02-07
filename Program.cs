d0edb689829f94b87ebd7c8837ca2fea7b283c50
Evan Comtesse <ComtesseE1@s2.rpn.ch>
2024-02-07 16:50:56 +0100

    Mise en place des panels principaux, environnement 3D

--- Program.cs
+++ Program.cs
@@ -1,1 +1,72 @@
-﻿
+﻿using static Raylib_cs.Raylib;
+using static RayGUI_cs.RayGUI;
+using Raylib_cs;
+using RayGUI_cs;
+using System.Numerics;
+
+namespace Uniray
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            // Initialize window and set mode
+            InitWindow(GetMonitorWidth(0), GetMonitorHeight(0), "Uniray");
+            SetConfigFlags(ConfigFlags.ResizableWindow);
+            SetConfigFlags(ConfigFlags.MaximizedWindow);
+
+            int wWindow = GetScreenWidth();
+            int hWindow = GetScreenHeight();
+
+            // Set 3D camera for the scene
+            Camera3D camera = new Camera3D();
+            camera.Projection = CameraProjection.Perspective;
+            camera.Position = Vector3.UnitY * 5;
+            camera.Target = Vector3.UnitX;
+            camera.Up = Vector3.UnitY;
+            camera.FovY = 90;
+
+            // ===============================================================================================================================================
+            // =============================================================== SET APPLICATION UI ============================================================
+            // ===============================================================================================================================================
+            
+            float cont1X = wWindow - wWindow / 1.25f;
+            float cont1Y = hWindow - hWindow / 3;
+            Container fileManager = new Container((int)cont1X, (int)cont1Y, wWindow - (int)cont1X, hWindow - (int)cont1Y, Uniray.APPLICATION_COLOR, Uniray.FOCUS_COLOR);
+            fileManager.Type = ContainerType.FileDropper;
+            fileManager.OutputFilePath = "assets/models/";
+            fileManager.ExtensionFile = "m3d";
+
+            Container gameManager = new Container(10, 30, (int)cont1X - 20, hWindow - 10, Uniray.APPLICATION_COLOR, Uniray.FOCUS_COLOR);
+
+            // Set FPS
+            SetTargetFPS(60);
+            while (!WindowShouldClose())
+            {
+                UpdateCamera(ref camera, CameraMode.FirstPerson);
+                BeginDrawing();
+
+                ClearBackground(Color.White);
+
+                // ===============================================================================================================================================
+                // =============================================================== MANAGE 3D SPACE ===============================================================
+                // ===============================================================================================================================================
+                BeginMode3D(camera);
+
+                DrawGrid(10, 10);
+
+                EndMode3D();
+                // ===============================================================================================================================================
+                // ============================================================= MANAGE APPLICATION ==============================================================
+                // ===============================================================================================================================================
+                DrawRectangle(0, 0, (int)cont1X, hWindow, Uniray.APPLICATION_COLOR);
+                DrawRectangle(0, (int)cont1Y - 10, wWindow, hWindow - (int)cont1Y + 10, Uniray.APPLICATION_COLOR);
+                DrawContainer(ref fileManager);
+                DrawContainer(ref gameManager);
+
+                EndDrawing();
+            }
+            CloseWindow();
+        }
+    }
+}
---
+++ Uniray.cs
@@ -0,0 +1,9 @@
+﻿using Raylib_cs;
+namespace Uniray
+{
+    public partial struct Uniray
+    {
+        public static readonly Color APPLICATION_COLOR = new Color(30, 30, 30, 255);
+        public static readonly Color FOCUS_COLOR = new Color(60, 60, 60, 255);
+    }
+}
