3c602cf5a2f1cbe7b05f71df6cca593cb1f5d00f
Evan Comtesse <ComtesseE1@s2.rpn.ch>
2024-02-26 16:07:01 +0100

    Implementation : Affichage et construction du UI depuis la classe Uniray

--- Program.cs
+++ Program.cs
@@ -12,9 +12,12 @@ namespace Uniray
         {
             // Initialize window and set mode
             InitWindow(1800, 900, "Uniray");
+            InitGUI(Uniray.APPLICATION_COLOR, Uniray.FOCUS_COLOR);
 
             int wWindow = GetScreenWidth();
             int hWindow = GetScreenHeight();
+
+            Uniray uniray = new Uniray(wWindow, hWindow);
 
             // Set 3D camera for the scene
             Camera3D camera = new Camera3D();
@@ -24,28 +27,8 @@ namespace Uniray
             camera.Up = Vector3.UnitY;
             camera.FovY = 90;
 
-            // ===============================================================================================================================================
-            // =============================================================== SET APPLICATION UI ============================================================
-            // ===============================================================================================================================================
-
-            Font baseFont = LoadFont("data/font/Ubuntu-Regular.ttf");
-
-            float cont1X = wWindow - wWindow / 1.25f;
-            float cont1Y = hWindow - hWindow / 3;
-            Container fileManager = new Container((int)cont1X, (int)cont1Y, wWindow - (int)cont1X, hWindow - (int)cont1Y, Uniray.APPLICATION_COLOR, Uniray.FOCUS_COLOR);
-            fileManager.Type = ContainerType.FileDropper;
-            fileManager.OutputFilePath = "assets/models/";
-            fileManager.ExtensionFile = "m3d";
-
-            Container gameManager = new Container(10, 30, (int)cont1X - 20, hWindow - 10, Uniray.APPLICATION_COLOR, Uniray.FOCUS_COLOR);
-
-            Textbox textbox = new Textbox("Hello World !", 140, 200, 50, 20, Uniray.APPLICATION_COLOR, Uniray.FOCUS_COLOR);
-            Button button = new Button("Evan", 1200, 400, 60, 20, Uniray.APPLICATION_COLOR, Uniray.FOCUS_COLOR);
-            button.Type = ButtonType.PathFinder;
-
             // Set FPS
             SetTargetFPS(60);
-
 
             /*List<int> keys = new List<int>();
             int index = 0;
@@ -73,12 +56,7 @@ namespace Uniray
                 // ===============================================================================================================================================
                 // ============================================================= MANAGE APPLICATION ==============================================================
                 // ===============================================================================================================================================
-                DrawRectangle(0, 0, (int)cont1X, hWindow, Uniray.APPLICATION_COLOR);
-                DrawRectangle(0, (int)cont1Y - 10, wWindow, hWindow - (int)cont1Y + 10, Uniray.APPLICATION_COLOR);
-                DrawContainer(ref fileManager);
-                DrawContainer(ref gameManager);
-                DrawTextbox(ref textbox, baseFont);
-                DrawButton(button, baseFont);
+                uniray.DrawUI();
 
                 EndDrawing();
             }
--- Uniray.cs
+++ Uniray.cs
@@ -1,9 +1,82 @@
-﻿using Raylib_cs;
+﻿using static Raylib_cs.Raylib;
+using Raylib_cs;
+using RayGUI_cs;
 namespace Uniray
 {
     public partial struct Uniray
     {
         public static readonly Color APPLICATION_COLOR = new Color(30, 30, 30, 255);
         public static readonly Color FOCUS_COLOR = new Color(60, 60, 60, 255);
+
+        private int hWindow;
+
+        private int wWindow;
+
+        private Font baseFont;
+
+        private List<Container> containers;
+
+        private List<Textbox> textboxes;
+
+        private List<Button> buttons;
+
+        /// <summary>
+        /// Construct UI
+        /// </summary>
+        public Uniray(int WWindow, int HWindow)
+        {
+            this.wWindow = WWindow;
+            this.hWindow = HWindow;
+
+            // Instantiate lists of components
+            containers = new List<Container>();
+            textboxes = new List<Textbox>();
+            buttons = new List<Button>();
+
+            // Load font
+            this.baseFont = LoadFont("data/font/Ubuntu-Regular.ttf");
+
+            // Containers
+            float cont1X = wWindow - wWindow / 1.25f;
+            float cont1Y = hWindow - hWindow / 3;
+            Container fileManager = new Container((int)cont1X, (int)cont1Y, wWindow - (int)cont1X, hWindow - (int)cont1Y - 10, APPLICATION_COLOR, FOCUS_COLOR);
+            fileManager.Type = ContainerType.FileDropper;
+            fileManager.OutputFilePath = "assets/models/";
+            fileManager.ExtensionFile = "m3d";
+            containers.Add(fileManager);
+
+            Container gameManager = new Container(10, 10, (int)cont1X - 20, hWindow - 20, APPLICATION_COLOR, FOCUS_COLOR);
+            containers.Add(gameManager);
+
+            // Buttons
+            Button button = new Button("Evan", 1200, 400, 60, 20, APPLICATION_COLOR, FOCUS_COLOR);
+            button.Type = ButtonType.PathFinder;
+            buttons.Add(button);
+
+            // Textboxes
+            Textbox textbox = new Textbox("Hello World !", 140, 200, 50, 20, APPLICATION_COLOR, FOCUS_COLOR);
+            textboxes.Add(textbox);
+        }
+
+        /// <summary>
+        /// Draw user interface of the application
+        /// </summary>
+        public void DrawUI()
+        {
+            DrawRectangle(0, 0, (int)(wWindow - wWindow / 1.25f), hWindow, APPLICATION_COLOR);
+            DrawRectangle(0, hWindow - hWindow / 3 - 10, wWindow, hWindow - (hWindow - hWindow / 3) + 10, APPLICATION_COLOR);
+
+            foreach (Button button in buttons) { RayGUI.DrawButton(button, baseFont); }
+            for (int i = 0; i < containers.Count; i++)
+            {
+                Container container = containers[i];
+                containers[i] = RayGUI.DrawContainer(ref container);
+            }
+            for (int i = 0; i < textboxes.Count; i++) 
+            {
+                Textbox textbox = textboxes[i];
+                textboxes[i] = RayGUI.DrawTextbox(ref textbox, baseFont); 
+            }
+        }
     }
 }
